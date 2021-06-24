0b313089cee4dbbf27d410908974833de3dde1e1
dance2die <sungmkim80@gmail.com>
2021-06-24 04:29:55 -0400

    Added edge case tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,31 @@
 import group from '../src';
 
-test('happy path', () => {
+describe('Edge cases', () => {
+  const EMPTY_SPACE = "";
+
+  it('returns empty string for an empty input', () => {
+    expect(group``).toBe(EMPTY_SPACE);
+  })
+
+  it('returns empty string for space(s) input', () => {
+    const maxSpaceCount = 100;
+    const buildSpaces = (count: number) => Array(count).fill(' ').join(EMPTY_SPACE);
+    for (let spaceCount = 1; spaceCount <= maxSpaceCount; spaceCount++) {
+      const spaces = buildSpaces(spaceCount);
+      expect(group`${spaces}`).toBe(EMPTY_SPACE);
+    }
+  })
+
+  it('return empty space for whitespaces', () => {
+    expect(group`
+
+
+`).toBe(EMPTY_SPACE);
+  })
+})
+
+test('Dynamic Brackets', () => {
   // group`text-white sm:(text-black bg-blue) lg:(text-red bg-white)`;
-
-
   let brackets = ['(', ')']
   // brackets = ['{', '}']
   let classNames = `bg-red-800 p-2 text-white text-xs mt-4
@@ -28,6 +50,8 @@ test('happy path', () => {
       dark:${brackets[0]}bg-gray-800 text-red-100${brackets[1]}
     ${brackets[1]}`
 
+  const expected = 'bg-red-800 p-2 text-white text-xs mt-4 dark:text-gray-800 dark:bg-gray-300 sm:text-sm sm:p-4 sm:text-gray-100 sm:dark:bg-[#fff] sm:dark:text-white sm:dark:text-[30px] md:text-base md:p-8 md:bg-red-500 md:text-gray-100 md:dark:bg-gray-600 md:dark:text-red-100 lg:text-lg lg:p-12 lg:bg-red-300 lg:text-gray-700 lg:dark:bg-[#abcdef] lg:dark:text-red-100 xl:text-2xl xl:p-16 xl:bg-white xl:text-gray-800 xl:dark:bg-gray-800 xl:dark:text-red-100';
   const actual = group`${classNames}`
-  console.log({ classNames, actual })
+
+  expect(actual).toBe(expected);
 })
