efa46032f8221a935dd2e42c17cb9dba7ec46af5
dance2die <sungmkim80@gmail.com>
2021-06-24 05:14:28 -0400

    added a comment for a possible future impelementation

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,17 @@ import zipWith from 'lodash.zipwith';
 const joinTemplateStringsArray = (tailwindClassNames: TemplateStringsArray, ...args: any[]) =>
   zipWith(tailwindClassNames, args, (className, arg) => (className ?? "") + (arg ?? "")).join(' ')
 
+// Future version will accept brackets as an "option".
+// So you can do tw`tailwind classes` or tw(['(', '')])`tailwind classes`
+// function tw(option, ...args) {
+//   console.log({ option, brackets: tw.prototype.brackets });
+//   const fn = (text, ...rest) => console.log({ option, text, rest });
+
+//   if (!!option.raw) return fn(option, args);
+//   else return fn;
+
+//   throw Error("You can't do this!");
+// }
 
 function variantGroup(tailwindClassNames: TemplateStringsArray, ...args: any[]) {
   const brackets = ['(', ')'] as const;
